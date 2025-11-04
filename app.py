cca776f10912fcaeae49c241f4bc8bfa26bb55ed
shavel28 <shashava05@gmail.com>
2025-11-04 21:40:25 +0700

    tampilan login, lupa pss, reset, verifikasi otp

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/lupaPassword')
+def lupaPassword():
+    return render_template('lupaPassword.html')
+
+@app.route('/verifikasiOTP')
+def verifikasiOTP():
+    return render_template('verifikasiOTP.html')
+
+@app.route('/resetPss')
+def resetPss():
+    return render_template('resetPss.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
